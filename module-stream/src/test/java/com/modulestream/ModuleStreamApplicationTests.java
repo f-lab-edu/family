2e135b16c141c3881ad5cdf0cc50bfa09547f59c
이종훈 <jonghuncu@gmail.com>
2024-08-24 00:06:57 +0900

    init module (#1)
    * init module
    
    * #1 Chore: gitignore 수정
    
    - gitignore 에서 불필요한 내용 제거
    - 제거 항목 netBeans, STS, VS Code, yml, properties
    
    * #1 Fix: settings.gradle 하위 모듈 명시적으로 경로 설정
    
    * #1 Fix: build.gradle 모듈별 내용 수정
    
    - root build.gradle: java, Springboot 내용 제거, subproject, allproject 메서드 제거 -> 각 모듈별로 설정
    - core build.gradle: java 플러그인 적용
    - api build.gradle: java, Springboot 플러그인 적용
    - stream build.gradle: java, Springboot 플러그인 적용

---
+++ module-stream/src/test/java/com/modulestream/ModuleStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modulestream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
